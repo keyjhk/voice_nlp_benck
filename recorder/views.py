b1ef42c3112fed59a9381c468e88344d4b905e41
蒋慧凯 <2656391651@qq.com>
2022-01-19 19:56:30 +0800

    chunks合并

--- recorder/views.py
+++ recorder/views.py
@@ -6,42 +6,46 @@ from rest_framework.views import APIView
 from rest_framework import status
 from django.conf import settings
 
-
 # Create your views here.
+TEMP_DIR = os.path.join(settings.MEDIA_ROOT, 'temp')
+
 
 class RecorderView(APIView):
-    def merge_files(self, fname):
+    def merge_files(self, fname, chunks):
         # pfname:fname_part_xx
-        print('fname:{}'.format(fname))
+        print('chunks',chunks)
         media_root = settings.MEDIA_ROOT
-        part_files = filter(lambda pfname: fname in pfname, [file for file in os.listdir(media_root)])
-        part_files = sorted(part_files, key=lambda x: int(x.split('part_')[-1]))
+        temp_dir = TEMP_DIR
+        part_files = filter(lambda pfname: fname in pfname, [file for file in os.listdir(temp_dir)])
+        part_files = sorted(part_files, key=lambda x: int(x.split('part_')[-1]))[:chunks]
         # merge & remove
-        with open(os.path.join(media_root,fname), 'wb') as f:
+        with open(os.path.join(media_root, fname), 'wb') as f:
             for pf in part_files:
-                temp_file = open(os.path.join(media_root, pf), 'rb')
+                temp_file = open(os.path.join(temp_dir, pf), 'rb')
                 f.write(temp_file.read())
                 temp_file.close()
                 print('merge {}'.format(pf))
-                os.remove(os.path.join(settings.MEDIA_ROOT, pf))
+                os.remove(os.path.join(temp_dir, pf))
 
     def get(self, request):
         return Response('hello recorder!')
 
     def post(self, request):
         file = request.data['file']
-        # fname = str(int(time.time())) + '.wav'  # random file name
-        fname = file.name
-        with open(os.path.join(settings.MEDIA_ROOT, fname), 'wb') as f:
+        save_dir = TEMP_DIR if request.data.get('is_chunk') else settings.MEDIA_ROOT
+        if not os.path.exists(save_dir): os.mkdir(save_dir)
+        fname = file.name  # file object
+        save_dir = os.path.join(save_dir, fname)
+        with open(save_dir, 'wb') as f:
             for chunk in file.chunks(): f.write(chunk)
 
-        file_url = request.build_absolute_uri(settings.MEDIA_URL + fname)
+        file_url = request.build_absolute_uri(save_dir + fname)
         return Response({'url': file_url}, status=status.HTTP_200_OK)
 
     def put(self, request):
         # merge part files
-        file = request.data
-        fname = file['name']
-        self.merge_files(fname)
+        fname = request.data['name']
+        chunks = request.data['chunks']
+        self.merge_files(fname, chunks)
         file_url = request.build_absolute_uri(settings.MEDIA_URL + fname)
         return Response({'url': file_url}, status=status.HTTP_200_OK)
