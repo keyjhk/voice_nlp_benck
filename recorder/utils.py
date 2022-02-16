dea17e431f6d8551383a2e147a37ff2aab934756
tjp <946625178@qq.com>
2022-02-17 01:41:01 +0800

    后端修改为match

--- recorder/utils.py
+++ recorder/utils.py
@@ -67,6 +67,31 @@ def text_sim_new(text1, text2):
     else:
         return None
 
+def text_sim_match(text1, text2):
+
+    terms_reference = jieba.cut(text2)  # 默认精准模式
+    terms_model = jieba.cut(text1)
+
+
+    s1=set()
+    for i in terms_model:
+        s1.add(i)
+
+    s2 = set()
+    for i in terms_reference:
+        s2.add(i)
+
+    count=0
+    for i in list(s2):
+        if i in s1:
+           count=count+1
+
+    if count / len(s2)>0.7:
+        return  True
+
+    return False
+
+
 def voice2t(voice_path):
     os.system(
         '/home/wenet/runtime/server/x86/main.sh ' + voice_path + ' 2>&1 | tee /home/wenet/runtime/server/x86/log.txt')
@@ -83,5 +108,5 @@ def voice2t(voice_path):
 
 def judge_deal(answer, save_dir):
     text2 = voice2t(save_dir)
-    res = text_sim_new(answer, text2)
+    res = text_sim_match(text2,answer)
     return res
